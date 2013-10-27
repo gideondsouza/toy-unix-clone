a0fb93c344c6674ce43d43e6a5a5b77d4deb6f2b
Gideon Israel Dsouza <gidisrael@gmail.com>
2013-10-28 00:11:40 +0530

    Continued with chapter 3.
    Actually got it all working. Although I was trying to build a cross compiler
    I managed to cross compile the whole thing with regular gcc on a CentOS box.
    
    I used qemu instead of bochs and it works like a charm.

--- os/src/main.c
+++ os/src/main.c
@@ -1,8 +1,13 @@
 // main.c -- Defines the C-code kernel entry point, calls initialisation routines.
 //           Made for JamesM's tutorials <www.jamesmolloy.co.uk>
 
+#include "monitor.h"
+
 int main(struct multiboot *mboot_ptr)
 {
     // All our initialisation calls will go in here.
-    return 0xDEADBABA;
+  monitor_clear();
+  monitor_write("Hey there!!\n");
+  monitor_write("Hello beautiful world!");
+  return 0;
 }
